b25d0c9b7b9a598e348c73ecb084873d3ca61d84
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2025-04-01 16:35:17 -0400

    chore(cd): set -xe and fix values.yml path

--- .github/workflows/deploy/ladesa-production/deploy.sh
+++ .github/workflows/deploy/ladesa-production/deploy.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+set -xe;
+
 helm upgrade -i ${HELM_RELEASE_NAME} \
   --repo https://stakater.github.io/stakater-charts application --version 6.0.2 \
-  --namespace=${K8S_NAMESPACE} -f ./.github/workflows/deploy/values.prod.yml \
+  --namespace=${K8S_NAMESPACE} -f ./values.yml \
 ;
 
 kubectl rollout restart --namespace ${K8S_NAMESPACE} deployment.apps/${K8S_DEPLOYMENT};
