b35230b43ce6f359d05050a6ecfa86542af0bfa8
Gabriel R. Antunes <gabrielrodantunes.dev@gmail.com>
2026-01-18 20:52:48 -0400

    chore: ci/cd

--- .github/workflows/deploy/development/deploy.sh
+++ .github/workflows/deploy/development/deploy.sh
@@ -2,6 +2,10 @@
 
 set -xe;
 
+K8S_DEPLOYMENT=docs
+K8S_NAMESPACE=ladesa
+HELM_RELEASE_NAME=ladesa-docs
+
 helm upgrade -i ${HELM_RELEASE_NAME} \
   --repo https://stakater.github.io/stakater-charts application --version 6.0.2 \
   --namespace=${K8S_NAMESPACE} -f ./values.yml \
